1ebb3145f2a19164f07d05fcbcce7cba4fa8b5cb
Diego Veiga <sdiegoveiga@gmail.com>
2026-04-08 19:40:46 -0300

    feat: add signin and signout function with better auth

--- src/app/(board)/_components/header.tsx
+++ src/app/(board)/_components/header.tsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { LogInIcon, SearchIcon } from "lucide-react"
+import { Loader2Icon, LogInIcon, SearchIcon } from "lucide-react"
 import { debounce, parseAsString, useQueryState } from "nuqs"
 import { Input } from "@/components/input"
+import { authClient } from "@/lib/auth-client"
 
 export function Header() {
+  const { data: session, isPending } = authClient.useSession()
   const [search, setSearch] = useQueryState("q", parseAsString.withDefault(""))
 
   function handleSearchUpdate(event: React.ChangeEvent<HTMLInputElement>) {
@@ -12,6 +14,14 @@ export function Header() {
       limitUrlUpdates: event.target.value !== "" ? debounce(500) : undefined,
       shallow: false,
     })
+  }
+
+  async function handleSignIn() {
+    await authClient.signIn.social({ provider: "github", callbackURL: "/" })
+  }
+
+  async function handleSignOut() {
+    await authClient.signOut()
   }
 
   return (
@@ -35,12 +45,31 @@ export function Header() {
           />
         </div>
 
-        <button
-          type="button"
-          className="bg-navy-700 size-8 rounded-full border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
-        >
-          <LogInIcon className="size-3.5 text-navy-200" />
-        </button>
+        {isPending ? (
+          <div className="bg-navy-700 size-8 rounded-full border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150">
+            <Loader2Icon className="size-4 text-navy-200 animate-spin" />
+          </div>
+        ) : session?.user ? (
+          <button
+            type="button"
+            onClick={handleSignOut}
+            className="size-8 rounded-full overflow-hidden cursor-pointer"
+          >
+            <img
+              src={session.user.image ?? ""}
+              alt={session.user.name}
+              className="size-8 rounded-full"
+            />
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={handleSignIn}
+            className="bg-navy-700 size-8 rounded-full border border-navy-500 flex items-center justify-center hover:bg-navy-600 transition-colors duration-150"
+          >
+            <LogInIcon className="size-3.5 text-navy-200" />
+          </button>
+        )}
       </div>
     </div>
   )
