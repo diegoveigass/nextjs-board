2b5aba1d392a4f08a92f6225f43b2b76f214bba8
Diego Veiga <sdiegoveiga@gmail.com>
2026-04-11 15:25:31 -0300

    feat: adding scrollbar plugin and fetch issues in board page

---
+++ src/http/list-issues.ts
@@ -0,0 +1,10 @@
+import { IssuesListResponseSchema } from "@/api/routes/list-issues"
+
+export async function listIssues() {
+  const response = await fetch("http://localhost:3000/api/issues")
+  const data = await response.json()
+
+  const issues = IssuesListResponseSchema.parse(data)
+
+  return issues
+}
